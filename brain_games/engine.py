142a0deeef44b217e2a676a8fb53eb673a68e46e
danya <ustizkii@gmail.com>
2024-09-04 18:07:45 +0500

    update games

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,8 @@
 def run_game(game_module):
     """
     Запускает игру с переданным игровым модулем.
-    В качестве аргумента функции передается модуль игры, содержащий игровую логику и инструкции.
+    В качестве аргумента функции передается модуль игры, содержащий игровую
+    логику и инструкции.
     Функция выводит вопрос и проверяет правильность ответов игрока.
     Если игрок трижды отвечает правильно, он побеждает.
     Если ответ неверен, игра завершится с выводом правильного ответа.
@@ -19,7 +20,8 @@ def run_game(game_module):
         if user_answer == str(correct_answer):
             print("Correct!")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(. "
+                  "Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
 
